eddf5096a871b9fedceffe3293052c1b080096e1
Mario Oately <davidkalina@proton.me>
2025-01-31 20:43:23 -0700

    style: constrain max height of page details modal.

--- components/PageDetailsDialog.tsx
+++ components/PageDetailsDialog.tsx
@@ -134,7 +134,7 @@ const PageDetailsDialog = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-xs md:max-w-2xl bg-zinc-900 border-zinc-800 p-0">
+      <DialogContent className="max-h-[300px] md:max-h-[600px] overflow-y-auto max-w-xs md:max-w-2xl bg-zinc-900 border-zinc-800 p-0">
         <DialogHeader className="px-6 py-4 border-b border-zinc-800">
           <div className="flex items-center justify-between mb-4">
             <div className="flex items-center space-x-2">
